392481c4d56c5a87adcae8041d9343ac87ea68dc
LukaszJudasz <lukasz19911@gmail.com>
2017-02-25 22:44:51 +0100

    basic implementation of add item

--- app/controllers/warehouse_controller.rb
+++ app/controllers/warehouse_controller.rb
@@ -1,4 +1,25 @@
 class WarehouseController < ApplicationController
   def index
   end
+
+  def save_item # stores item in db
+    @item = Item.new(item_params)
+    @item.save
+
+    redirect_to warehouse_index_path
+  end
+
+  def add_item # show form for adding item
+    @item = Item.new
+  end
+
+  def get_item
+
+  end
+
+private
+def item_params
+  params.require(:item)
+        .permit(:name, :producent, :datasheet)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
 
+  get 'warehouse/add_item'
+  post 'warehouse/save_item', as: 'items'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
