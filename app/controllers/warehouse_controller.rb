194069b5261594484bdee46fa500df56ae7f5362
LukaszJudasz <lukasz19911@gmail.com>
2017-02-25 23:31:10 +0100

    create, read, delete items

--- app/controllers/warehouse_controller.rb
+++ app/controllers/warehouse_controller.rb
@@ -6,15 +6,26 @@ class WarehouseController < ApplicationController
     @item = Item.new(item_params)
     @item.save
 
-    redirect_to warehouse_index_path
+    redirect_to items_show_all_path
   end
 
   def add_item # show form for adding item
     @item = Item.new
   end
 
-  def get_item
+  def get_details
+    @item = Item.find(params[:id])
+  end
 
+  def delete_item
+    @item = Item.find(params[:id])
+    @item.destroy
+
+    redirect_to items_show_all_path
+  end
+
+  def show_all
+    @all_items = Item.all
   end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   get 'warehouse/add_item'
   post 'warehouse/save_item', as: 'items'
+  get 'warehouse/show_all', as: 'items_show_all'
+  get 'warehouse/item_details/:id', to: 'warehouse#get_details', as: 'item_details'
+  get 'warehouse/delete_item/:id', to: 'warehouse#delete_item', as: 'delete_item'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
