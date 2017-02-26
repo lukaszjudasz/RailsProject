df4552f813ce4b2e04eb98de24992cd3b560b24e
LukaszJudasz <lukasz19911@gmail.com>
2017-02-26 17:52:54 +0100

    code refactor

--- app/controllers/warehouse_controller.rb
+++ app/controllers/warehouse_controller.rb
@@ -2,29 +2,35 @@ class WarehouseController < ApplicationController
   def index
   end
 
-  def save_item # stores item in db
+  def save_item # action
     @item = Item.new(item_params)
     @item.save
 
-    redirect_to items_show_all_path
+    redirect_to show_all_items_path
   end
 
-  def add_item # show form for adding item
+  def add_item # view
     @item = Item.new
   end
 
-  def get_details
+  def get_details #view
     @item = Item.find(params[:id])
   end
 
-  def delete_item
+  def modify_item # action
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to show_all_items_path
+  end
+
+  def delete_item # action
     @item = Item.find(params[:id])
     @item.destroy
 
-    redirect_to items_show_all_path
+    redirect_to show_all_items_path
   end
 
-  def show_all
+  def show_all # view
     @all_items = Item.all
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
 
-  get 'warehouse/add_item'
-  post 'warehouse/save_item', as: 'items'
-  get 'warehouse/show_all', as: 'items_show_all'
-  get 'warehouse/item_details/:id', to: 'warehouse#get_details', as: 'item_details'
+  get 'warehouse/add_item', as: 'add_item'
+  post 'warehouse/save_item', as: 'create_item'
+  get 'warehouse/show_all', as: 'show_all_items'
+  get 'warehouse/item_details/:id', to: 'warehouse#get_details', as: 'get_item'
   get 'warehouse/delete_item/:id', to: 'warehouse#delete_item', as: 'delete_item'
+  patch 'warehouse/modify_item/:id', to: 'warehouse#modify_item', as: 'modify_item'
 
 
+  root 'warehouse#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
