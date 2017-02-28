8459bc307ec5f907dcddf19cfbfd3c9209e05701
LukaszJudasz <lukasz19911@gmail.com>
2017-02-28 19:02:14 +0100

    fixed routes, some more bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   get 'warehouse/add_item', as: 'add_item'
-  post 'warehouse/save_item', as: 'create_item'
+  post 'warehouse/add_item', as: 'create_item'
   get 'warehouse/show_all', as: 'show_all_items'
   get 'warehouse/item_details/:id', to: 'warehouse#get_details', as: 'get_item'
   get 'warehouse/delete_item/:id', to: 'warehouse#delete_item', as: 'delete_item'
